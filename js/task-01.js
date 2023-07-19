68a6efb3622581d0c504462aef7588b4660a3136
Viktoriia Yuziuk <tory.sheridan.28@gmail.com>
2023-07-19 11:46:15 +0300

    edit task 1

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,6 @@ const numberOfItems = listOfCategories.length;
 console.log(`Number of categories: ${numberOfItems}`);
 
 listOfCategories.forEach(item => {
-  console.log(`Category: ${item.querySelector('h2').textContent}`);
+  console.log(`Category: ${item.firstElementChild.textContent}`);
   console.log(`Elements: ${item.querySelectorAll('li').length}`);
 });
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const input = document.querySelector('input');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  amount = input.value;
+}
+
+function destroyBoxes() {}
